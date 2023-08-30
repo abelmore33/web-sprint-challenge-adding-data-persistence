9aaf264dfb3d0ee672f67050fbbec235385d8882
Aaron Belmore <belmoreaaron9@gmail.com>
2023-08-30 18:50:45 -0400

    completed server and index.js

---
+++ data/migrations/20230830222915_resources-table.js
@@ -0,0 +1,42 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name").notNullable();
+      tbl.string("project_description");
+      tbl.boolean("project_completed");
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name").notNullable().unique();
+      tbl.string("resource_description");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.string("task_description").notNullable();
+      tbl.string("task_notes");
+      tbl.boolean("task_completed");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
